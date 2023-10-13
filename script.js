9f05157a90e81793ccd5fcc6e7e2d47c066b9232
Bhargavi Vankayala <146963251+Bhargavi135@users.noreply.github.com>
2023-10-13 21:02:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+
+let var1 = 5
+let var2 = 6
+let var3 = 2
+
+console.log(var1+var2)
+
+console.log(var3-var2)
+
+console.log(var3/var1)
+
+console.log(var2*var3)
+
+let num1 = 2
+let num2 = 3
+let num3 = 4
+
+console.log(num1 ** num2)
+
+let biryani = 250
+biryani++; (biryani+1)
+
+console.log(biryani)
+
+biryani--;
+
+console.log(biryani)
+
+let apple = 20
+
+apple += 10
+
+console.log(apple)
+
+apple -= 1
+
+console.log(apple)
+
+apple /= 2
+
+console.log(apple)
+
+apple %= 6
+
+console.log(apple)
+
+apple **= 3
+
+console.log(apple)
+
+
+let number1 = 3
+let number2 = 6
+let number3 = 9
+
+console.log(number3 == number1)
+console.log(number1 === number2)
+console.log(number1 < number2)
+console.log(number1 > number3)
